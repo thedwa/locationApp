1252ec441743667376ce47cac944d8e366c5ec28
Theodore Wanner <wanner.theodore@gmail.com>
2023-05-17 13:34:23 +0200

    wip

--- public/app.js
+++ public/app.js
@@ -40,19 +40,19 @@ async function callOpenAPI() {
     try {
         let prompt;
         if (activeCategory === "history") {
-            prompt = `We are looking for historical information about ${cityName}. Please provide some details. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `We are looking for historical information about ${cityName}. Please provide some details. Provide about 70 words. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } else if (activeCategory === "politics") {
-            prompt = `Tell us about the political landscape in ${cityName}. What are the key political events or figures? After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `Tell us about the political landscape in ${cityName}. What are the key political events or figures? Provide about 70 words. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } else if (activeCategory === "culture") {
-            prompt = `Share some insights about the culture and traditions of ${cityName}. What makes it unique? After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `Share some insights about the culture and traditions of ${cityName}. What makes it unique? Provide about 70 words. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } else if (activeCategory === "funfact") {
-            prompt = `Give us an interesting fun fact about ${cityName}. Something that people may not know. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `Give us an interesting fun fact about ${cityName}. Something that people may not know. Provide about 70 words. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } else if (activeCategory === "activities") {
-            prompt = `What are some popular activities and attractions in ${cityName}? Provide a brief description. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `What are some popular activities and attractions in ${cityName}? Provide a brief description. Provide about 70 words. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } else if (activeCategory === "food") {
-            prompt = `Describe the local cuisine of ${cityName}. What are some famous dishes or culinary specialties? After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `Describe the local cuisine of ${cityName}. What are some famous dishes or culinary specialties? Provide about 70 words. after that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } else if (activeCategory === "nearby") {
-            prompt = `What are some fun things to do nearby or in ${cityName}. Anything that I should not miss? After that, suggest 2-4 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+            prompt = `What are some fun things to do nearby or in ${cityName}. Anything that I should not miss? Provide about 70 words. After that, suggest 2-4 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
         } 
 
         console.log("Prompt:", prompt);
@@ -93,3 +93,57 @@ async function callOpenAPI() {
 // Add event listener to the search button
 const searchButton = document.getElementById("citySearchBtn");
 searchButton.addEventListener("click", search);
+
+
+
+//generate the image:
+async function generateImage() {
+    try {
+
+        let prompt;
+        if (activeCategory === "history") {
+            prompt = `A historical image of the city of ${cityName}`;
+        } else if (activeCategory === "politics") {
+            prompt = `A political image of the city of ${cityName}`;
+        } else if (activeCategory === "culture") {
+            prompt = `A cusltural image of the city of ${cityName}`;
+        } else if (activeCategory === "funfact") {
+            prompt = `A beautiful image of the city of ${cityName}`;
+        } else if (activeCategory === "activities") {
+            prompt = `What are some popular activities and attractions in ${cityName}? Provide a brief description. After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+        } else if (activeCategory === "food") {
+            prompt = `Describe the local cuisine of ${cityName}. What are some famous dishes or culinary specialties? After that, suggest 2-3 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+        } else if (activeCategory === "nearby") {
+            prompt = `What are some fun things to do nearby or in ${cityName}. Anything that I should not miss? After that, suggest 2-4 areas where you can tell more about from the text. Use the following format: 1. tell me more about XXX`;
+        } 
+        console.log("Prompt:", prompt);
+
+        const data = {
+            prompt: prompt,
+            n: 1,
+            size: '512x512',
+            response_format: 'url'
+        };
+
+        const response = await fetch('/api/generateImage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+
+        const json = await response.json();
+        const imageUrl = json.url;
+
+        // Display the generated image
+        const generatedImage = document.getElementById("generated-image");
+        generatedImage.src = imageUrl;
+
+        // Show the image container
+        const imageContainer = document.getElementById("image-container");
+        imageContainer.style.display = "block";
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
--- src/server.js
+++ src/server.js
@@ -34,6 +34,31 @@ app.post('/api/generateLocationInfo', async (req, res) => {
 });
 
 
+app.post('/api/generateImage', async (req, res) => {
+  const data = req.body;
+
+  try {
+    const fetch = (await import('node-fetch')).default;
+    const response = await fetch('https://api.openai.com/v1/images/generations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    const json = await response.json();
+    res.send(json);
+    console.log(json);
+  } catch (error) {
+    res.status(500).send({ message: 'Error making request to DALL·E API' });
+  }
+});
+
+
+
+
 //start the port
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
